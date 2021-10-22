85e652c822cfd82bac06b0f07c2fd0a6987aed3a
MorpDev <89537270+MorpDev@users.noreply.github.com>
2021-10-23 01:28:08 +0200

    Update snipe.py

--- snipe.py
+++ snipe.py
@@ -1,22 +1,44 @@
-async def discord_user(user_id):
-    header= {"Yetki":"Bot [token, ayrıca Bot bir bot hesabıysa başlangıçta orada olurdu]"}
-    a = aiohttp.ClientSession(headers=header)
-    response = await a.get(f"https://discord.com/api/v9/users/{user_id}",headers=header)
-    response =  await response.json()
-    if str(response["avatar"]).startswith("a_"):
-        avatar = "https://cdn.discordapp.com/avatars/{0}/{1}.gif?size=1024".format(user_id,response["avatar"])
-    else:
-        avatar = "https://cdn.discordapp.com/avatars/{0}/{1}.png?size=1024".format(user_id,response["avatar"])
-
-    if response["banner"] == None:
-        banner = None
-    elif str(response["banner"]).startswith("a_"):
-        banner = "https://cdn.discordapp.com/banners/{0}/{1}.gif?size=1024".format(user_id,response["banner"])
-    else:
-        banner = "https://cdn.discordapp.com/banners/{0}/{1}.png?size=1024".format(user_id,response["banner"])
-    await a.close()
-    a = response
-    a["avatar"] = avatar
-    a["banner"] = banner
-    return a
+@bot.event
+async def on_message_delete(message):
+  bot.deleted.append(message.content)
+  bot.author.append(message.author.id)
+  bot.time.append(datetime.datetime.timestamp(datetime.datetime.now()))
+@bot.command()
+async def snipe(ctx, i:int = None):
+  if bot.deleted==[]:
+    await ctx.send("Hiç Bir mesaj silinmedi!")
+    return
+  if i==None:
+    f=len(bot.deleted)
+    i=len(bot.deleted)-1
+    a=bot.get_user(bot.author[i])
+    e=bot.deleted[i]
+    ts=round(bot.time[i])
+    embed=discord.Embed(description=e)
+    embed.set_author(name=f"Snipe 1/{f}", icon_url=a.avatar_url)
+    embed.add_field(name='** **', value=f'Mesajı Silen **{a.name}#{a.discriminator}** on <t:{ts}:f>')
+    embed.set_footer(text=f'Kullanan Kişi {ctx.author.name}#{ctx.author.discriminator}', icon_url=ctx.author.avatar_url)
+    await ctx.send(embed=embed)
+  elif int(len(bot.deleted))>=i and i!=0:
+    u=i
+    f=len(bot.deleted)
+    i=len(bot.deleted)-i
+    a=bot.get_user(bot.author[i])
+    e=bot.deleted[i]
+    ts=round(bot.time[i])
+    embed=discord.Embed(description=e)
+    embed.set_author(name=f"Snipe {u}/{f}", icon_url=a.avatar_url)
+    embed.set_footer(text=f'Requested by {ctx.author.name}#{ctx.author.discriminator}', icon_url=ctx.author.avatar_url)
+    embed.add_field(name='** **', value=f'Mesajı Silen **{a.name}#{a.discriminator}** on <t:{ts}:f>')
+    await ctx.send(embed=embed)
+  elif i==0:
+    await ctx.send("Geçerli bir sayı girin, 0 beklenen bir bağımsız değişken değil...")
+  else:
+    f=len(bot.deleted)
+    await ctx.send(f"sadece benim tarafımdan izlenen `{f}` silinmiş mesajlar var")
+    return
+@snipe.error
+async def snipe_error(ctx, error):
+  if isinstance(error, commands.BadArgument):
+    await ctx.send("En son silinen mesajı istiyorsanız sayı alanını boş bırakın veya gerçek bir sayı girin!")
  
